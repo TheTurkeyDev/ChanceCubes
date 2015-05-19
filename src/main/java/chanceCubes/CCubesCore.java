da2ebdd40f4503b9a84e30733765e6a210158b74
tterrag1098 <tterrag1098@gmail.com>
2015-05-19 11:53:27 -0400

    Add EnderCore

--- src/main/java/chanceCubes/CCubesCore.java
+++ src/main/java/chanceCubes/CCubesCore.java
@@ -6,6 +6,8 @@ import net.minecraft.item.Item;
 
 import org.apache.logging.log4j.Logger;
 
+import com.enderio.core.IEnderMod;
+
 import chanceCubes.blocks.CCubesBlocks;
 import chanceCubes.proxy.CommonProxy;
 import chanceCubes.registry.ChanceCubeRegistry;
@@ -19,49 +21,68 @@ import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 
 @Mod(modid = CCubesCore.MODID, version = CCubesCore.VERSION)
-public class CCubesCore
+public class CCubesCore implements IEnderMod
 {
-	public static final String MODID = "chancecubes";
-	public static final String VERSION = "0.1";
-	public static final String NAME = "Chance Cubes";
+    public static final String MODID = "chancecubes";
+    public static final String VERSION = "0.1";
+    public static final String NAME = "Chance Cubes";
 
-	@Instance(value = MODID)
-	public static CCubesCore instance;
-	@SidedProxy(clientSide="chanceCubes.proxy.ClientProxy", serverSide="chanceCubes.proxy.CommonProxy")
-	public static CommonProxy proxy;
-	public static CreativeTabs modTab = new CreativeTabs(MODID) {
-		public Item getTabIconItem()
-		{
-			return Item.getItemFromBlock(CCubesBlocks.chanceCube);
-		}
-	};
-	public static Logger logger;
-	
-	public static ChanceCubeRegistry cCubeRegistry;
+    @Instance(value = MODID)
+    public static CCubesCore instance;
+    @SidedProxy(clientSide = "chanceCubes.proxy.ClientProxy", serverSide = "chanceCubes.proxy.CommonProxy")
+    public static CommonProxy proxy;
+    public static CreativeTabs modTab = new CreativeTabs(MODID)
+    {
+        public Item getTabIconItem()
+        {
+            return Item.getItemFromBlock(CCubesBlocks.chanceCube);
+        }
+    };
+    public static Logger logger;
 
-	@EventHandler
-	public void init(FMLInitializationEvent event)
-	{
+    public static ChanceCubeRegistry cCubeRegistry;
 
-	}
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
 
-	@EventHandler
-	public void load(FMLPreInitializationEvent event)
-	{
-		logger = event.getModLog();
+    }
 
-		CCubesBlocks.loadBlocks();
-		cCubeRegistry = new ChanceCubeRegistry();
-		cCubeRegistry.loadDefaultRewards();
+    @EventHandler
+    public void load(FMLPreInitializationEvent event)
+    {
+        logger = event.getModLog();
 
-		ConfigLoader.loadConfigSettings(event.getSuggestedConfigurationFile());
-		FMLCommonHandler.instance().bus().register(new UpdateNotificationHandler());
-	}
+        CCubesBlocks.loadBlocks();
+        cCubeRegistry = new ChanceCubeRegistry();
+        cCubeRegistry.loadDefaultRewards();
 
-	@EventHandler
-	public void postInit(FMLPostInitializationEvent event)
-	{
-		Blocks.hardened_clay.setHardness(0.3F);
-		Blocks.stained_hardened_clay.setHardness(0.3F);
-	}
+        ConfigLoader.loadConfigSettings(event.getSuggestedConfigurationFile());
+        FMLCommonHandler.instance().bus().register(new UpdateNotificationHandler());
+    }
+
+    @EventHandler
+    public void postInit(FMLPostInitializationEvent event)
+    {
+        Blocks.hardened_clay.setHardness(0.3F);
+        Blocks.stained_hardened_clay.setHardness(0.3F);
+    }
+
+    @Override
+    public String modid()
+    {
+        return MODID;
+    }
+
+    @Override
+    public String name()
+    {
+        return NAME;
+    }
+
+    @Override
+    public String version()
+    {
+        return VERSION;
+    }
 }
