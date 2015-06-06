4605ed8e70f6217ed2f7c4dddeeb2f44c8e08b69
Turkey <turkey2349@gmail.com>
2015-06-05 23:28:38 -0400

    - Fixed the AnvilRain reward

--- src/main/java/chanceCubes/rewards/AnvilRain.java
+++ src/main/java/chanceCubes/rewards/AnvilRain.java
@@ -10,25 +10,27 @@ import chanceCubes.CCubesCore;
 public class AnvilRain implements IChanceCubeReward
 {
 	private Random rand = new Random();
+	
 	@Override
 	public void trigger(World world, int x, int y, int z, EntityPlayer player)
 	{
+		int x1 = x + (rand.nextInt(9) - 4);
+		int z1 = z + (rand.nextInt(9) - 4);
 
-		int x1 = rand.nextInt(12);
-		int z1 = rand.nextInt(12);
+		int x2 = x + (rand.nextInt(9) - 4);
+		int z2 = z + (rand.nextInt(9) - 4);
 
-		int x2 = rand.nextInt(12);
-		int z2 = rand.nextInt(12);
+		int x3 = x + (rand.nextInt(9) - 4);
+		int z3 = z + (rand.nextInt(9) - 4);
 
-		int x3 = rand.nextInt(12);
-		int z3 = rand.nextInt(12);
+		int x4 = x + (rand.nextInt(9) - 4);
+		int z4 = z + (rand.nextInt(9) - 4);
 
-		int x4 = rand.nextInt(12);
-		int z4 = rand.nextInt(12);
+		int x5 = x + (rand.nextInt(9) - 4);
+		int z5 = z + (rand.nextInt(9) - 4);
 
-		int x5 = rand.nextInt(12);
-		int z5 = rand.nextInt(12);
-
+		for(int yy = 0; yy < 25; yy++)
+			world.setBlockToAir(x, y+yy, z);
 		for(int yy = 0; yy < 25; yy++)
 			world.setBlockToAir(x1, y+yy, z1);
 		for(int yy = 0; yy < 25; yy++)
@@ -40,6 +42,7 @@ public class AnvilRain implements IChanceCubeReward
 		for(int yy = 0; yy < 25; yy++)
 			world.setBlockToAir(x5, y+yy, z5);
 		
+		world.setBlock(x, y+25, z, Blocks.anvil);
 		world.setBlock(x1, y+25, z1, Blocks.anvil);
 		world.setBlock(x2, y+25, z2, Blocks.anvil);
 		world.setBlock(x3, y+25, z3, Blocks.anvil);
