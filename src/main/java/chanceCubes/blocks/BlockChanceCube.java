b662c6a413f1c2bf47c2792e2159aa82d05eb1c3
Turkey <turkey2349@gmail.com>
2015-09-01 21:33:57 -0400

    - Chance Cubes are now blast resistant - Added CaptainSparklez to the custom user reward list

--- src/main/java/chanceCubes/blocks/BlockChanceCube.java
+++ src/main/java/chanceCubes/blocks/BlockChanceCube.java
@@ -5,10 +5,14 @@ import java.util.Random;
 import net.minecraft.block.Block;
 import net.minecraft.block.ITileEntityProvider;
 import net.minecraft.block.material.Material;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.boss.EntityWither;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.world.Explosion;
+import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
 import net.minecraftforge.common.util.FakePlayer;
 import chanceCubes.CCubesCore;
@@ -18,7 +22,7 @@ import chanceCubes.registry.ChanceCubeRegistry;
 import chanceCubes.tileentities.TileChanceCube;
 
 public class BlockChanceCube extends Block implements ITileEntityProvider
-{ 
+{
 	public BlockChanceCube()
 	{
 		super(Material.ground);
@@ -37,20 +41,20 @@ public class BlockChanceCube extends Block implements ITileEntityProvider
 	@Override
 	public boolean removedByPlayer(World world, EntityPlayer player, int x, int y, int z, boolean willHarvest)
 	{
-		if (!world.isRemote && player != null && !(player instanceof FakePlayer))
+		if(!world.isRemote && player != null && !(player instanceof FakePlayer))
 		{
 			TileChanceCube te = (TileChanceCube) world.getTileEntity(x, y, z);
 			if(player.inventory.getCurrentItem() != null && player.inventory.getCurrentItem().getItem().equals(CCubesItems.silkPendant))
 			{
 				ItemStack stack = new ItemStack(Item.getItemFromBlock(CCubesBlocks.chanceCube), 1);
-				((ItemChanceCube)stack.getItem()).setChance(stack, te.getChance());
+				((ItemChanceCube) stack.getItem()).setChance(stack, te.getChance());
 				this.dropBlockAsItem(world, x, y, z, stack);
 				world.setBlockToAir(x, y, z);
 				world.removeTileEntity(x, y, z);
 				return true;
 			}
 
-			if (te != null)
+			if(te != null)
 			{
 				world.setBlockToAir(x, y, z);
 				ChanceCubeRegistry.INSTANCE.triggerRandomReward(world, x, y, z, player, te.getChance());
@@ -63,5 +67,23 @@ public class BlockChanceCube extends Block implements ITileEntityProvider
 	public int quantityDropped(Random p_149745_1_)
 	{
 		return 0;
+	}
+
+	@Override
+	public boolean canEntityDestroy(IBlockAccess world, int x, int y, int z, Entity entity)
+	{
+		return !(entity instanceof EntityWither) && super.canEntityDestroy(world, x, y, z, entity);
+	}
+
+	@Override
+	public void onBlockExploded(World world, int x, int y, int z, Explosion explosion)
+	{
+
+	}
+
+	@Override
+	public boolean canDropFromExplosion(Explosion explosion)
+	{
+		return false;
 	}
 }
