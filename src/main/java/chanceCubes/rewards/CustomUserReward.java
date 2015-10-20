d0419dda4ca159b4b66db717d4425bd8b40d19eb
Turkey <turkey2349@gmail.com>
2015-10-20 18:32:40 -0400

    - Fixed a bug with too many requests to gameprofile api when adding a custom reward

--- src/main/java/chanceCubes/rewards/CustomUserReward.java
+++ src/main/java/chanceCubes/rewards/CustomUserReward.java
@@ -2,7 +2,9 @@ package chanceCubes.rewards;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.Map.Entry;
+import java.util.UUID;
 
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.item.EntityItem;
@@ -10,6 +12,7 @@ import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.ChatComponentText;
 import net.minecraft.world.World;
+import net.minecraftforge.common.UsernameCache;
 
 import org.apache.logging.log4j.Level;
 
@@ -27,7 +30,7 @@ import com.google.gson.JsonElement;
 public class CustomUserReward implements IChanceCubeReward
 {
 	private String userName = "";
-	private String uuid = "";
+	private UUID uuid = null;
 	private String type;
 
 	private List<BasicReward> customRewards = new ArrayList<BasicReward>();
@@ -47,12 +50,18 @@ public class CustomUserReward implements IChanceCubeReward
 			return;
 		}
 
+		UUID uuidTemp = this.getPlayerUUID(player.getCommandSenderName());
+		if(uuidTemp == null)
+		{
+			CCubesCore.logger.log(Level.ERROR, "Chance Cubes failed to get the uuid of the user!");
+			return;
+		}
 		for(JsonElement user : users.getAsJsonArray())
 		{
-			if(user.getAsJsonObject().get("UUID").getAsString().equalsIgnoreCase(player.getUniqueID().toString()))
+			if(user.getAsJsonObject().get("UUID").getAsString().equalsIgnoreCase(uuidTemp.toString()))
 			{
 				userName = user.getAsJsonObject().get("Name").getAsString();
-				uuid = player.getUniqueID().toString();
+				uuid = uuidTemp;
 				type = user.getAsJsonObject().get("Type").getAsString();
 			}
 		}
@@ -88,14 +97,15 @@ public class CustomUserReward implements IChanceCubeReward
 	@Override
 	public void trigger(final World world, final int x, final int y, final int z, final EntityPlayer player)
 	{
-		if(!uuid.equalsIgnoreCase(player.getUniqueID().toString()))
+
+		if(!UsernameCache.getLastKnownUsername(uuid).equalsIgnoreCase(player.getCommandSenderName()))
 		{
 			player.addChatMessage(new ChatComponentText("Hey you aren't " + this.userName + "! You cant have his reward! Try again!"));
 			Entity itemEnt = new EntityItem(world, x, y, z, new ItemStack(CCubesBlocks.chanceCube, 1));
 			world.spawnEntityInWorld(itemEnt);
 			return;
 		}
-		
+
 		player.addChatMessage(new ChatComponentText("Selecting best (possibly deadly) reward for " + this.type + " " + this.userName));
 
 		Task task = new Task("Custom Reward", 100)
@@ -125,6 +135,18 @@ public class CustomUserReward implements IChanceCubeReward
 	public String getName()
 	{
 		return CCubesCore.MODID + ":CR_" + this.userName;
+	}
+
+	public UUID getPlayerUUID(String username)
+	{
+		for(Map.Entry<UUID, String> entry : UsernameCache.getMap().entrySet())
+		{
+			if(entry.getValue().equalsIgnoreCase(username))
+			{
+				return entry.getKey();
+			}
+		}
+		return null;
 	}
 
 }
