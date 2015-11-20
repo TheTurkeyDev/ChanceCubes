e2a4b651590666ebbc4f981a24f3c1e492e50b92
Turkey <turkey2349@gmail.com>
2015-11-19 22:23:52 -0500

    - Reworked the Clear inventory reward

--- src/main/java/chanceCubes/rewards/ClearInventoryReward.java
+++ src/main/java/chanceCubes/rewards/ClearInventoryReward.java
@@ -1,50 +1,54 @@
 package chanceCubes.rewards;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.entity.player.InventoryPlayer;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.ChatComponentText;
 import net.minecraft.world.World;
 import chanceCubes.CCubesCore;
-import chanceCubes.items.ItemChanceCube;
-import chanceCubes.util.Scheduler;
-import chanceCubes.util.Task;
 
 public class ClearInventoryReward implements IChanceCubeReward
 {
+	private Random rand = new Random();
 
 	@Override
 	public void trigger(World world, int x, int y, int z, final EntityPlayer player)
 	{
-		boolean cubes = false;
-		final InventoryPlayer inv = player.inventory;
-		for(int slotNum = 0; slotNum < player.inventory.getSizeInventory(); slotNum++)
+		List<Integer> validSlots = new ArrayList<Integer>();
+		ItemStack[] mainInv = player.inventory.mainInventory;
+		ItemStack[] armorInv = player.inventory.armorInventory;
+		for(int i = 0; i < mainInv.length + armorInv.length; i++)
 		{
-			if(player.inventory.getStackInSlot(slotNum) != null && player.inventory.getStackInSlot(slotNum).getItem() instanceof ItemChanceCube)
-				cubes = true;
-			else
-				player.inventory.setInventorySlotContents(slotNum, null);
-		}
-		
-		world.playSoundEffect(x, y, z, "random.burp", 1, 1);
-		player.addChatMessage(new ChatComponentText("I hope you didn't have anything of value with you :)"));
-		if(cubes)
-			player.addChatMessage(new ChatComponentText("Don't worry, I left the cubes for you!"));
-		
-		if(world.rand.nextInt(5) == 1)
-		{
-			Task task = new Task("Replace_Inventory", 200)
+			if(i < mainInv.length)
 			{
-				@Override
-				public void callback()
-				{
-					player.inventory = inv;
-					player.addChatMessage(new ChatComponentText("AHHHHHH JK!! You should have seen your face!"));
-				}
-
-			};
-
-			Scheduler.scheduleTask(task);
+				if(mainInv[i] != null)
+					validSlots.add(i);
+			}
+			else
+			{
+				if(armorInv[i - mainInv.length] != null)
+					validSlots.add(i);
+			}
 		}
+
+		int toRemove = rand.nextInt(5) + 1;
+
+		for(int i = 0; i < toRemove; i++)
+		{
+			if(validSlots.size() > 0)
+			{
+				int randomint = rand.nextInt(validSlots.size());
+				int slot = validSlots.get(randomint);
+				player.inventory.setInventorySlotContents(slot, null);
+				validSlots.remove(randomint);
+			}
+		}
+
+		player.addChatMessage(new ChatComponentText("The Cubes have spoken!"));
+		player.addChatMessage(new ChatComponentText(toRemove + " item(s) were removed from your inventory!"));
 	}
 
 	@Override
