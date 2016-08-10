43405debb08a3f538262864fa7d37b878ddd569d
Turkey <turkey2349@gmail.com>
2016-08-10 02:08:24 -0400

    - Changed crafting recipies requiring Tier 2 Pendent to use tier 1

--- src/main/java/chanceCubes/CraftingRecipies.java
+++ src/main/java/chanceCubes/CraftingRecipies.java
@@ -20,8 +20,8 @@ public class CraftingRecipies
 		GameRegistry.addShapedRecipe(new ItemStack(CCubesItems.chancePendantT2, 1), "GBG", "BPB", "GBG", 'P', new ItemStack(CCubesItems.chancePendantT1), 'B', new ItemStack(Blocks.lapis_block), 'G', new ItemStack(Blocks.gold_block));
 		GameRegistry.addShapedRecipe(new ItemStack(CCubesItems.chancePendantT3, 1), "GBG", "BPB", "GBG", 'P', new ItemStack(CCubesItems.chancePendantT2), 'B', new ItemStack(Blocks.lapis_block), 'G', new ItemStack(Blocks.gold_block));
 
-		GameRegistry.addShapedRecipe(new ItemStack(CCubesItems.silkPendant, 1), "SBS", "SPS", "SBS", 'P', new ItemStack(CCubesItems.chancePendantT2), 'B', new ItemStack(Blocks.lapis_block), 'S', new ItemStack(Items.string));
+		GameRegistry.addShapedRecipe(new ItemStack(CCubesItems.silkPendant, 1), "SBS", "SPS", "SBS", 'P', new ItemStack(CCubesItems.chancePendantT1), 'B', new ItemStack(Blocks.lapis_block), 'S', new ItemStack(Items.string));
 
-		GameRegistry.addShapedRecipe(new ItemStack(CCubesItems.scanner, 1), "IGI", "GPG", "IGI", 'P', new ItemStack(CCubesItems.chancePendantT2), 'G', new ItemStack(Blocks.glass), 'I', new ItemStack(Items.iron_ingot));
+		GameRegistry.addShapedRecipe(new ItemStack(CCubesItems.scanner, 1), "IGI", "GPG", "IGI", 'P', new ItemStack(CCubesItems.chancePendantT1), 'G', new ItemStack(Blocks.glass), 'I', new ItemStack(Items.iron_ingot));
 	}
 }
