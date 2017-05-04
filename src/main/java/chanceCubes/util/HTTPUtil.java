5e5bad31ef1480941839838a283d3d1357cbd372
Turkey <turkey2349@gmail.com>
2017-05-04 15:27:43 -0400

    - Fixed bad code

--- src/main/java/chanceCubes/util/HTTPUtil.java
+++ src/main/java/chanceCubes/util/HTTPUtil.java
@@ -1,15 +1,16 @@
 package chanceCubes.util;
 
 import java.io.BufferedInputStream;
+import java.io.DataOutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
 import org.apache.logging.log4j.Level;
 
-import chanceCubes.CCubesCore;
-
 import com.google.gson.JsonElement;
 import com.google.gson.JsonParser;
+
+import chanceCubes.CCubesCore;
 
 public class HTTPUtil
 {
@@ -19,30 +20,43 @@ public class HTTPUtil
 	public static JsonElement getWebFile(String link, CustomEntry<String, String>... extras) throws Exception
 	{
 		HttpURLConnection con = (HttpURLConnection) new URL(link).openConnection();
-		con.setDoOutput(false);
+		con.setDoOutput(true);
 		con.setReadTimeout(5000);
 		con.setRequestProperty("Connection", "keep-alive");
 		con.setRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:16.0) Gecko/20100101 Firefox/16.0");
-		
-		for(CustomEntry<String, String> property: extras)
-			con.setRequestProperty(property.getKey(), property.getValue());
-		
 		((HttpURLConnection) con).setRequestMethod("GET");
 		con.setConnectTimeout(5000);
-		
+
+		StringBuilder builder = new StringBuilder();
+
+		for(CustomEntry<String, String> property : extras)
+		{
+			builder.append(property.getKey());
+			builder.append("=");
+			builder.append(property.getValue());
+			builder.append("&");
+		}
+
+		builder.deleteCharAt(builder.length() - 1);
+
+		DataOutputStream wr = new DataOutputStream(con.getOutputStream());
+		wr.writeBytes(builder.toString());
+		wr.flush();
+		wr.close();
+
 		BufferedInputStream in = new BufferedInputStream(con.getInputStream());
 		int responseCode = con.getResponseCode();
-		
+
 		if(responseCode != HttpURLConnection.HTTP_OK && responseCode != HttpURLConnection.HTTP_MOVED_PERM)
 			CCubesCore.logger.log(Level.WARN, "Update request returned response code: " + responseCode + " " + con.getResponseMessage());
 		else if(responseCode == HttpURLConnection.HTTP_MOVED_PERM)
 			throw new Exception();
 
-		StringBuffer buffer = new StringBuffer();
+		StringBuilder buffer = new StringBuilder();
 		int chars_read;
 		while((chars_read = in.read()) != -1)
 			buffer.append((char) chars_read);
-		
+
 		String page = buffer.toString();
 
 		return json.parse(page);
