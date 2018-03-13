ea6fc832cf218f16a029c9070d47135d0bc20eda
Turkey <turkey2349@gmail.com>
2018-03-12 22:14:27 -0400

    Well thanks ChefLethalPlays

--- src/main/java/chanceCubes/blocks/BlockCompactGiantCube.java
+++ src/main/java/chanceCubes/blocks/BlockCompactGiantCube.java
@@ -1,7 +1,11 @@
 package chanceCubes.blocks;
 
+import java.util.Random;
+
+import chanceCubes.config.CCubesSettings;
 import chanceCubes.sounds.CCubesSounds;
 import chanceCubes.util.GiantCubeUtil;
+import chanceCubes.util.RewardsUtil;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.item.ItemStack;
@@ -20,10 +24,17 @@ public class BlockCompactGiantCube extends BaseChanceBlock
 	public void onBlockPlacedBy(World world, BlockPos pos, IBlockState state, EntityLivingBase placer, ItemStack stack)
 	{
 		super.onBlockPlacedBy(world, pos, state, placer, stack);
-		if(world.isRemote)
+		if(world.isRemote || RewardsUtil.isBlockUnbreakable(world, pos.add(0, 1, 0)) && CCubesSettings.nonReplaceableBlocks.contains(world.getBlockState(pos.add(0, 1, 0))))
 			return;
-		GiantCubeUtil.setupStructure(pos.add(-1, 0, -1), world, true);
 		
+		GiantCubeUtil.setupStructure(pos.add(-1, 0, -1), world, true);
+
 		world.playSound(null, pos.getX(), pos.getY(), pos.getZ(), CCubesSounds.GIANT_CUBE_SPAWN.getSoundEvent(), CCubesSounds.GIANT_CUBE_SPAWN.getSoundCategory(), 1.0F, 1.0F);
+	}
+	
+	@Override
+	public int quantityDropped(Random rand)
+	{
+		return 1;
 	}
 }
