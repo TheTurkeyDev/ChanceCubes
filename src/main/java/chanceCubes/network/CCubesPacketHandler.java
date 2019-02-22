f253fb31fbca1c759226883fc24db458c1145d67
Turkey2349 <turkey2349@gmail.com>
2019-02-21 19:07:14 -0500

    Found Network/ Packet code

--- src/main/java/chanceCubes/network/CCubesPacketHandler.java
+++ src/main/java/chanceCubes/network/CCubesPacketHandler.java
@@ -1,20 +1,32 @@
 package chanceCubes.network;
 
 import chanceCubes.CCubesCore;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.fml.network.FMLPlayMessages;
+import net.minecraftforge.fml.network.NetworkHooks;
 import net.minecraftforge.fml.network.NetworkRegistry;
 import net.minecraftforge.fml.network.simple.SimpleChannel;
 
 public class CCubesPacketHandler
 {
-	public static final SimpleChannel INSTANCE = NetworkRegistry.newSimpleChannel(CCubesCore.MODID);
 	private static int id = 0;
-	
+	public static final SimpleChannel INSTANCE = NetworkRegistry.ChannelBuilder
+	            .named(new ResourceLocation(CCubesCore.MODID, "packets"))
+	            .clientAcceptedVersions(a -> true)
+	            .serverAcceptedVersions(a -> true)
+	            .networkProtocolVersion(() -> NetworkHooks.NETVERSION)
+	            .simpleChannel();
+
 	public static void init()
 	{
-		INSTANCE.registerMessage(PacketCreativePendant.Handler.class, PacketCreativePendant.class, id++, Side.SERVER);
+		INSTANCE.registerMessage(id++, PacketCreativePendant.class, PacketCreativePendant::encode, PacketCreativePendant::decode, PacketCreativePendant::handle);
+		
 		INSTANCE.registerMessage(PacketRewardSelector.Handler.class, PacketRewardSelector.class, id++, Side.SERVER);
 		INSTANCE.registerMessage(PacketTriggerD20.Handler.class, PacketTriggerD20.class, id++, Side.CLIENT);
 		INSTANCE.registerMessage(PacketParticle.Handler.class, PacketParticle.class, id++, Side.CLIENT);
 		INSTANCE.registerMessage(PacketCubeScan.Handler.class, PacketCubeScan.class, id++, Side.SERVER);
+		
+		//Reference
+		//INSTANCE.registerMessage(id++, FMLPlayMessages.OpenContainer.class, FMLPlayMessages.OpenContainer::encode, FMLPlayMessages.OpenContainer::decode, FMLPlayMessages.OpenContainer::handle);
 	}
 }
--- src/main/java/chanceCubes/network/PacketCreativePendant.java
+++ src/main/java/chanceCubes/network/PacketCreativePendant.java
@@ -1,18 +1,22 @@
 package chanceCubes.network;
 
-import io.netty.buffer.ByteBuf;
-import net.minecraftforge.common.network.ForgeMessage;
-import net.minecraftforge.fml.common.network.ByteBufUtils;
+import java.util.function.Supplier;
 
-public class PacketCreativePendant extends ForgeMessage
+import org.apache.logging.log4j.Level;
+
+import chanceCubes.CCubesCore;
+import chanceCubes.containers.CreativePendantContainer;
+import chanceCubes.items.ItemChanceCube;
+import net.minecraft.inventory.Container;
+import net.minecraft.item.ItemStack;
+import net.minecraft.network.PacketBuffer;
+import net.minecraftforge.fml.network.NetworkEvent;
+
+public class PacketCreativePendant
 {
 
 	private String playerName;
 	private int chancevalue;
-
-	public PacketCreativePendant()
-	{
-	}
 
 	public PacketCreativePendant(String player, int chance)
 	{
@@ -20,44 +24,39 @@ public class PacketCreativePendant extends ForgeMessage
 		this.chancevalue = chance;
 	}
 
-	public void toBytes(ByteBuf buf)
+	public static void encode(PacketCreativePendant msg, PacketBuffer buf)
 	{
-		ByteBufUtils.writeUTF8String(buf, playerName);
-		buf.writeInt(chancevalue);
-
+		buf.writeString(msg.playerName);
+		buf.writeInt(msg.chancevalue);
 	}
 
-	public void fromBytes(ByteBuf buf)
+	public static PacketCreativePendant decode(PacketBuffer buf)
 	{
-		this.playerName = ByteBufUtils.readUTF8String(buf);
-		this.chancevalue = buf.readInt();
-
+		return new PacketCreativePendant(buf.readString(32767), buf.readInt());
 	}
 
-//	public static final class Handler implements IMessageHandler<PacketCreativePendant, IMessage>
-//	{
-//		@Override
-//		public IMessage onMessage(PacketCreativePendant message, MessageContext ctx)
-//		{
-//			Container c;
-//			try
-//			{
-//				c = ctx.getServerHandler().player.openContainer;
-//			} catch(Exception NullPointerException)
-//			{
-//				CCubesCore.logger.log(Level.ERROR, "Chance Cubes has failed to set the chance of a cube due to a packet failure! Please Inform Turkey of this!");
-//				return null;
-//			}
-//
-//			if(c instanceof CreativePendantContainer)
-//			{
-//				CreativePendantContainer container = (CreativePendantContainer) c;
-//				ItemStack ccubes = container.getChanceCubesInPendant();
-//				if(!ccubes.isEmpty() && ccubes.getItem() instanceof ItemChanceCube)
-//					((ItemChanceCube) ccubes.getItem()).setChance(ccubes, message.chancevalue);
-//			}
-//			return null;
-//		}
-//	}
+	public static void handle(PacketCreativePendant msg, Supplier<NetworkEvent.Context> ctx)
+	{
+		ctx.get().enqueueWork(() -> {
+			Container c;
+			try
+			{
+				c = ctx.get().getSender().openContainer;
+			} catch(Exception NullPointerException)
+			{
+				CCubesCore.logger.log(Level.ERROR, "Chance Cubes has failed to set the chance of a cube due to a packet failure! Please Inform Turkey of this!");
+				return;
+			}
+
+			if(c instanceof CreativePendantContainer)
+			{
+				CreativePendantContainer container = (CreativePendantContainer) c;
+				ItemStack ccubes = container.getChanceCubesInPendant();
+				if(!ccubes.isEmpty() && ccubes.getItem() instanceof ItemChanceCube)
+					((ItemChanceCube) ccubes.getItem()).setChance(ccubes, msg.chancevalue);
+			}
+		});
+		ctx.get().setPacketHandled(true);
+	}
 
 }
