c9b5972ad489910779eb425ee45483f797384859
Turkey2349 <turkey2349@gmail.com>
2019-03-04 17:09:26 -0500

    Forgot to add the event to the method

--- src/main/java/chanceCubes/CCubesCore.java
+++ src/main/java/chanceCubes/CCubesCore.java
@@ -41,6 +41,7 @@ import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.client.registry.ClientRegistry;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
 import net.minecraftforge.fml.event.server.FMLServerStartingEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
@@ -66,13 +67,14 @@ public class CCubesCore
 
 	public CCubesCore()
 	{
+		FMLJavaModLoadingContext.get().getModEventBus().addListener(this::commonStart);
 		FMLJavaModLoadingContext.get().getModEventBus().addListener(this::clientStart);
 		FMLJavaModLoadingContext.get().getModEventBus().addListener(this::serverStart);
 		FMLJavaModLoadingContext.get().getModEventBus().addListener(this::onIMCMessage);
 		ModLoadingContext.get().registerExtensionPoint(ExtensionPoint.GUIFACTORY, () -> CCubesGuiHandler::openGui);
 	}
 
-	public void commonStart()
+	public void commonStart(FMLCommonSetupEvent event)
 	{
 		CCubesSounds.loadSolunds();
 		CCubesRecipies.loadRecipies();
