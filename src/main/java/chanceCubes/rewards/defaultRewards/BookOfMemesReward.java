c3225989b6a77420dba86cc09d1fa59a7cfc0d22
A_D <A-UNDERSCORE-D@users.noreply.github.com>
2020-04-02 02:41:50 +0200

    Fix compile warnings regarding utf8 in code
    Python magic is magic.
    
    ```
    import json
    
    lines = r"""memes.add("Sodium, atomic number 11, was first isolated by Peter Dager in 1807. A chemical component of salt, he named it Na in honor of the saltiest region on earth, North America.");
    memes.add("(╯°□°）╯︵ ┻━┻ \n ༼ᕗຈل͜ຈ༽ᕗ RAISE YOUR DONGERS ༼ᕗຈل͜ຈ༽ᕗ");
    memes.add("Darude- status \n ☐ Not Sandstorm \n ☑ Sandstorm");
    memes.add("( ͡° ͜ʖ ͡°) Every 60 seconds in Africa, a minute passes. Together we can stop this. Please spread the word ( ͡° ͜ʖ ͡°) ");
    memes.add("YESTERDAY YOU SAID TOMMOROW, Don't let your dreams be memes, Don't meme your dreams be beams, Jet fuel won't melt tomorrow's memes, DON'T LET YOUR STEEL MEMES BE JET DREAMS");
    memes.add("If the human body is 75% water, how can you be 100% salt?");
    memes.add(" ▼ ◄ ▲ ► ▼ ◄ ▲ ► ▼ ◄ ▲ ► ▼ ◄ ▲ ► ▼ ◄ ▲ ▼ ◄ ▲ ► ▼ ◄ ▲ ► ▼ ◄▼ ◄ ▲ ► ▼ ◄ ▲ ► ▼ \n Sorry, I've dropped my bag of Doritos™ brand chips▲ ► ▼ ◄ ▲ ► ▼ ◄ ▲ ▼ ◄ ▲ ► ▼ ◄ ▲ ► ▼ ◄ ▲ ► ▼ ◄ ▲ ► ▼ ► ▼ ◄ ◄ ▲▲ ► ▼ ◄▼ ◄ ◄ ▼ ◄ ▲ ► ▼ ◄ ▲ ► ▼ ◄ ▲ ► ▼ ◄ ▲ ►");
    memes.add("Hey Chat....... \n \n \n \n 123");
    memes.add("O-oooooooooo AAAAE-A-A-I-A-U- JO-oooooooooooo AAE-O-A-A-U-U-A- E-eee-ee-eee AAAAE-A-E-I-E-A-JO-ooo-oo-oo-oo EEEEO-A-AAA-AAAA \n O-oooooooooo AAAAE-A-A-I-A-U- JO-oooooooooooo AAE-O-A-A-U-U-A- E-eee-ee-eee AAAAE-A-E-I-E-A-JO-ooo-oo-oo-oo EEEEO-A-AAA-AAAA");
    memes.add("Did you just assume this books meme? #Triggered");""".replace(r'memes.add("', "").replace(r'");', "").split("\n")
    
    for line in lines:
        escaped = json.dumps(line).replace(r"\\n", r"\n")
        print(f'\t\tmemes.add({escaped});')
    ```

--- src/main/java/chanceCubes/rewards/defaultRewards/BookOfMemesReward.java
+++ src/main/java/chanceCubes/rewards/defaultRewards/BookOfMemesReward.java
@@ -19,12 +19,12 @@ public class BookOfMemesReward extends BaseCustomReward
 	{
 		super(CCubesCore.MODID + ":book_of_memes", 0);
 		memes.add("Sodium, atomic number 11, was first isolated by Peter Dager in 1807. A chemical component of salt, he named it Na in honor of the saltiest region on earth, North America.");
-		memes.add("(╯°□°）╯︵ ┻━┻ \n ༼ᕗຈل͜ຈ༽ᕗ RAISE YOUR DONGERS ༼ᕗຈل͜ຈ༽ᕗ");
-		memes.add("Darude- status \n ☐ Not Sandstorm \n ☑ Sandstorm");
-		memes.add("( ͡° ͜ʖ ͡°) Every 60 seconds in Africa, a minute passes. Together we can stop this. Please spread the word ( ͡° ͜ʖ ͡°) ");
+		memes.add("(\u256f\u00b0\u25a1\u00b0\uff09\u256f\ufe35 \u253b\u2501\u253b \n \u0f3c\u1557\u0e88\u0644\u035c\u0e88\u0f3d\u1557 RAISE YOUR DONGERS \u0f3c\u1557\u0e88\u0644\u035c\u0e88\u0f3d\u1557");
+		memes.add("Darude- status \n \u2610 Not Sandstorm \n \u2611 Sandstorm");
+		memes.add("( \u0361\u00b0 \u035c\u0296 \u0361\u00b0) Every 60 seconds in Africa, a minute passes. Together we can stop this. Please spread the word ( \u0361\u00b0 \u035c\u0296 \u0361\u00b0) ");
 		memes.add("YESTERDAY YOU SAID TOMMOROW, Don't let your dreams be memes, Don't meme your dreams be beams, Jet fuel won't melt tomorrow's memes, DON'T LET YOUR STEEL MEMES BE JET DREAMS");
 		memes.add("If the human body is 75% water, how can you be 100% salt?");
-		memes.add(" ▼ ◄ ▲ ► ▼ ◄ ▲ ► ▼ ◄ ▲ ► ▼ ◄ ▲ ► ▼ ◄ ▲ ▼ ◄ ▲ ► ▼ ◄ ▲ ► ▼ ◄▼ ◄ ▲ ► ▼ ◄ ▲ ► ▼ \n Sorry, I've dropped my bag of Doritos™ brand chips▲ ► ▼ ◄ ▲ ► ▼ ◄ ▲ ▼ ◄ ▲ ► ▼ ◄ ▲ ► ▼ ◄ ▲ ► ▼ ◄ ▲ ► ▼ ► ▼ ◄ ◄ ▲▲ ► ▼ ◄▼ ◄ ◄ ▼ ◄ ▲ ► ▼ ◄ ▲ ► ▼ ◄ ▲ ► ▼ ◄ ▲ ►");
+		memes.add(" \u25bc \u25c4 \u25b2 \u25ba \u25bc \u25c4 \u25b2 \u25ba \u25bc \u25c4 \u25b2 \u25ba \u25bc \u25c4 \u25b2 \u25ba \u25bc \u25c4 \u25b2 \u25bc \u25c4 \u25b2 \u25ba \u25bc \u25c4 \u25b2 \u25ba \u25bc \u25c4\u25bc \u25c4 \u25b2 \u25ba \u25bc \u25c4 \u25b2 \u25ba \u25bc \n Sorry, I've dropped my bag of Doritos\u2122 brand chips\u25b2 \u25ba \u25bc \u25c4 \u25b2 \u25ba \u25bc \u25c4 \u25b2 \u25bc \u25c4 \u25b2 \u25ba \u25bc \u25c4 \u25b2 \u25ba \u25bc \u25c4 \u25b2 \u25ba \u25bc \u25c4 \u25b2 \u25ba \u25bc \u25ba \u25bc \u25c4 \u25c4 \u25b2\u25b2 \u25ba \u25bc \u25c4\u25bc \u25c4 \u25c4 \u25bc \u25c4 \u25b2 \u25ba \u25bc \u25c4 \u25b2 \u25ba \u25bc \u25c4 \u25b2 \u25ba \u25bc \u25c4 \u25b2 \u25ba");
 		memes.add("Hey Chat....... \n \n \n \n 123");
 		memes.add("O-oooooooooo AAAAE-A-A-I-A-U- JO-oooooooooooo AAE-O-A-A-U-U-A- E-eee-ee-eee AAAAE-A-E-I-E-A-JO-ooo-oo-oo-oo EEEEO-A-AAA-AAAA \n O-oooooooooo AAAAE-A-A-I-A-U- JO-oooooooooooo AAE-O-A-A-U-U-A- E-eee-ee-eee AAAAE-A-E-I-E-A-JO-ooo-oo-oo-oo EEEEO-A-AAA-AAAA");
 		memes.add("Did you just assume this books meme? #Triggered");
