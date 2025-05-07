27802f2d16cd8c875b9272cfae567d983df3a3e0
Mrbysco <Mrbysco@users.noreply.github.com>
2025-05-07 21:42:03 +0200

    Remove unused import

--- src/main/java/chanceCubes/commands/CCubesServerCommands.java
+++ src/main/java/chanceCubes/commands/CCubesServerCommands.java
@@ -27,7 +27,6 @@ import net.minecraft.commands.arguments.EntityArgument;
 import net.minecraft.commands.arguments.coordinates.BlockPosArgument;
 import net.minecraft.core.BlockPos;
 import net.minecraft.nbt.Tag;
-import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.sounds.SoundSource;
